e458fec9f3156194407842ba11a49587bdeb3f57
Switorik <turek.c@gmail.com>
2020-04-26 16:58:54 -0400

    oops

--- src/main/java/switorik/commandblocksleepcmd/cmdListener.java
+++ src/main/java/switorik/commandblocksleepcmd/cmdListener.java
@@ -1,5 +1,6 @@
 package switorik.commandblocksleepcmd;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -22,7 +23,7 @@ public class cmdListener implements CommandExecutor {
             if (sender instanceof BlockCommandSender) {
 
                 if (args.length >= 5) {
-                    //format will be /cmdblockdelay x y z time setblock replacementblock:optional
+                    //format will be /cmdblockdelay x y z time setblock replacementblock:optional replacementtime:optional
 
                     while (true) {
 
@@ -30,13 +31,15 @@ public class cmdListener implements CommandExecutor {
                         int y = 0;
                         int z = 0;
                         int time = 0;
+//                        int replaceTime = 0;
                         Material material = Material.AIR;
-                        Material replacement = Material.AIR;
+//                        Material replacement = Material.AIR;
                         Block block = ((BlockCommandSender) sender).getBlock();
                         Location commandLoc = block.getLocation();
                         Location setBlock = new Location(block.getWorld(), x, y, z);
                         //setting up initial variables
 
+                        //parse X
                         if (isInteger(args[0])) {
                             x = Integer.parseInt(args[0]);
                         } else {
@@ -50,6 +53,8 @@ public class cmdListener implements CommandExecutor {
                                 break;
                             }
                         }
+
+                        //parse Y
                         if (isInteger(args[1])) {
                             y = Integer.parseInt(args[1]);
                         } else {
@@ -63,6 +68,8 @@ public class cmdListener implements CommandExecutor {
                                 break;
                             }
                         }
+
+                        //parse Z
                         if (isInteger(args[2])) {
                             z = Integer.parseInt(args[2]);
                         } else {
@@ -77,6 +84,7 @@ public class cmdListener implements CommandExecutor {
                             }
                         }
 
+                        //parse Time
                         if (isInteger(args[3])) {
                             time = Integer.parseInt(args[3]);
                             if (time < 0) {
@@ -85,16 +93,32 @@ public class cmdListener implements CommandExecutor {
                         } else {
                             break;
                         }
-                        if (isMaterial(args[4])) {
-                            material = Material.getMaterial(args[4]);
-                        } else {
+
+                        //check if it has a replacement time and parse it
+//                        if (!args[6].isEmpty() ) {
+//                            if (isInteger(args[6])) {
+//                                if (Integer.parseInt(args[6]) > time) {
+//                                    replaceTime = Integer.parseInt(args[6]);
+//                                } else {
+//                                    replaceTime = time + 1;
+//                                }
+//                            } else {
+//                                replaceTime = time + 1;
+//                            }
+//                        } else {
+//                            replaceTime = time + 1;
+//                        }
+                        //sender.sendMessage(replaceTime + "");
+                        //make sure we have a material to set
+                        if (!isMaterial(args[4])) {
                             break;
                         }
-
+                        //storing block location
                         setBlock.setX(x);
                         setBlock.setY(y);
                         setBlock.setZ(z);
 
+                        //convert MS to S
                         time = time / 1000;
 
                         plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
@@ -109,16 +133,18 @@ public class cmdListener implements CommandExecutor {
                         if (!args[5].isEmpty()) {
 
                             if (isMaterial(args[5])) {
-                                replacement = Material.getMaterial(args[5]);
 
-                                plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+                               //plugin.getServer().getLogger().info("Replace Time:" + replaceTime);
+
+                                //replaceTime = replaceTime / 1000;
+                                Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
                                     public void run() {
 
                                         setBlock.getBlock().setType(Material.getMaterial(args[5]));
 
                                     }
 
-                                }, (time * 20) + 1);
+                                }, time * 20);
 
                             } else {
                                 break;
